d24ad9ba3ce9c647a48662f31f6861c21726aa04
Naren Surampudi <nsurampu@gmail.com>
2018-11-09 19:04:14 +0530

    Some changes

--- json_read.py
+++ json_read.py
@@ -25,21 +25,6 @@ ts_matrix = []
 adjacency_matrix = {}
 # train_count = 0
 # station_count = 0
-'''
-for train in trains:
-    if train != "sample_train":
-        t_code = trains_data[train]['id']
-        speed = int(trains_data[train]['speed'])
-        s_time = trains_data[train]['start_time']
-        s_station = trains_data[train]['starting_station']
-        s_code = stations_data[s_station]['station_code']
-        for station_count in range(0, len(stations) - 1):
-            distance_dict = stations_data[s_station]['distances'][station_count]
-            distance_key = list(distance_dict.keys())[0]
-            distance = int(distance_dict[distance_key])
-            time = distance / speed
-            ts_matrix.append([[t_code], [distance_key], [time]])
-'''
 
 for train in trains:
     if train != 'sample_train':
